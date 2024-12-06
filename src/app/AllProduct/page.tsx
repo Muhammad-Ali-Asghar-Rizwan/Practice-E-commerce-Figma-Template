cf6d50bacb8ee36544ad110bfa33be985087e320
Muhammad-Ali-Asghar-Rizwan <programingmaster22@gmail.com>
2024-12-07 01:15:38 +0500

    update

--- src/app/AllProduct/page.tsx
+++ src/app/AllProduct/page.tsx
@@ -87,7 +87,7 @@ function AllProduct() {
             Exploring the World of AI and Technology
           </h1> */}
           <div className="grid grid-cols-1 mt-8 sm:grid-cols-2 lg:grid-cols-4 gap-8">
-            {posts.map((post, index) => (
+            {posts.map((post) => (
               <div className="fade-in" key={post.id}>
 
                 <AllProductCard post={post} />
